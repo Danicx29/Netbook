1992b664277db90f65b04549baeea91614ab7821
Amilcrack <amijose22@hotmail.com>
2018-08-08 11:54:41 -0600

    fhfgh

--- dashboard/account/index.php
+++ dashboard/account/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once("../../app/views/dashboard/templates/page.class.php");
 Page::login_header();
+
+
 require_once("../../app/controllers/dashboard/account/login__controllers.php");
+
+
 Page::login_footer();
 ?>
--- dashboard/menu/menu.php
+++ dashboard/menu/menu.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../../app/views/dashboard/templates/page.class.php");
 Page::templateHeader("Autor y editorial");
+
+
 require_once("../../app/controllers/dashboard/menu/menu__controllers.php");
 Page::templateFooter();
 ?>
